6e8e00513abf6e5a9fabdeac017300082da52c9f
Nurmuhamad <nurmuhamad1201f@gmail.com>
2024-05-16 02:26:09 +0500

    react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className=" text-[50px] font-sans font-[700] text-blue-600 xs:text-[20px] xs:text-red-300">Helloo World</h1>
+    </div>
+  )
+}
+
+export default App
